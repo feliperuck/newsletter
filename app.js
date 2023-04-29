4280dcbf4879a2f961f0e0fb430fa1d7c5d3f18c
Felipe Ruck <feliperuck@live.com>
2023-04-29 00:28:04 -0300

    Change on app.listen

--- app.js
+++ app.js
@@ -52,7 +52,7 @@ app.post("/failure", function (req, res) {
     res.redirect("/");
 });
 
-app.listen(3000, function () {
+app.listen(process.env.PORT, function () {
     console.log('listening on port 3000');
 })
 
